13af75d6d4519a1621cb85161edf448e1adb8841
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-12 02:43:10 +0530

    docs: add code comments for better readability and structure

---
+++ Python-Fundamentals/day02_comments.py
@@ -0,0 +1,15 @@
+#       comment
+
+# 1- single line comment
+#2 - multiline comment
+
+# wap to print 'hello world'
+print('hello world')
+
+'''wap to print your college
+    and your name
+    and your age'''
+
+print('bansal college of eng.')
+print('naveen')
+print('age')
