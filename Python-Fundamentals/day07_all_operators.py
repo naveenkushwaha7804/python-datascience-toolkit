1dd240cf78f4d176a8d3315ba93325af0bd6e7f4
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-16 23:13:39 +0530

    Add Python operators practice (arithmetic, logical, membership, bitwise, list ops)

---
+++ Python-Fundamentals/day07_all_operators.py
@@ -0,0 +1,77 @@
+x=645
+y=10
+# print(x+y)
+# print(x*y)
+# print(y/x)
+# print(x-y)
+# print(y%x)
+# print(x//10) #floor
+
+# print(x%10)
+# print(x**y) #power
+# print(y:=20) #valrus statement yah kuchh return nahi karta
+x=x/1
+print(x)
+
+# print(x>y) and (x<y)
+# print(x>y) or (x<y)
+# print(not(x>y))
+
+s='Python'
+# print('P' not in s)
+# print('P'  in s)
+# false
+
+l=[10,20,30,'python','java']
+# print(2 not in l)
+# true
+
+d={'name':'naveen' ,'age':20}
+# print('name' in d)
+#true
+# print('qualification' in d)
+#false
+# print('qualification' not in d)
+#true
+
+set={20 , 40 , 'naveen' , 12}
+# print(20 in set)
+# print(34 in  set)
+#true , false
+# print(20 in set)
+#true
+
+m=20
+n=30
+# print(m is n)
+#False
+# print(m is not n)
+#True
+# Here (is , is not ) work with memory address
+# and (== , != ) work with comparison value
+
+#BITWISE OPERATOR
+
+i=10
+j=20
+# print(i&j) #AND 
+# print(i|j) #OR
+k=7
+# print(k<<1)
+# print(k<<2)
+
+    #OPERATOR ON LIST
+
+l2=[10,20,30,'python' , 'java' , 80]
+l3=[20,60,30,'naveen' , 'java' , 100]
+
+
+# print(l2-l3)  # not supported
+print(l2*3)
+print(l2+l3)
+# print(l2/l3) #not supported
+# print(l2%l3)  #not supported
+# print(l2//l3) #not supported
+
+
+
