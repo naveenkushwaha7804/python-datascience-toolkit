65ff98dfff720ce7b3f3d659ad726bc7113f7c7e
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-16 23:26:19 +0530

    Add Python operators practice (arithmetic, logical, membership, bitwise, list ops)

---
+++ Python-Fundamentals/day08_python_keywords.py
@@ -0,0 +1,9 @@
+import keyword
+all_keys=keyword.kwlist
+all_softkeys=keyword.softkwlist
+total_keys=all_keys+all_softkeys
+print(all_keys)
+print(len(all_keys))
+print(all_softkeys)
+print(len(all_softkeys))
+print(total_keys)
