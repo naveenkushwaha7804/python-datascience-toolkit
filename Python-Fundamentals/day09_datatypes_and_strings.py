37a36c7b7bec97ae01d07618bcd63daf65974cbb
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-16 23:44:05 +0530

    Add Python datatypes practice (int, float, complex, strings)

---
+++ Python-Fundamentals/day09_datatypes_and_strings.py
@@ -0,0 +1,28 @@
+x=5+3j 
+#imp for interviev
+a=3
+y=20.4
+print(x ,a,y)
+print(type(x) ,type(a) , type(y))
+
+# x=0 -> empty integer
+#y=0.0 -> empty float
+#z=0j -> empty complex
+x=0
+y=0.0
+z=0j
+print(type(x), x )
+print(type(y), y)
+print(type(z), z)
+
+# 2. Collections of cherectors
+# Represented by ' ' , " " , '''  ''',
+# single quote (' ') or (" ") for single line
+# triple quote ('''  ''') for multiple cote
+
+s1='python'
+s2="python"
+s3='''python'''
+s4="this iss 'cybrom' coaching"
+print(s4)
+print(type(s1) , type(s2) , type(s3))
