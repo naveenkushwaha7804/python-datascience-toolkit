81a281edb4c1d3df0168b4feca0bcc8048bb2f36
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-17 21:08:32 +0530

    Add Python type conversion examples

---
+++ Python-Fundamentals/day11_type_casting.py
@@ -0,0 +1,12 @@
+# x=10.5
+# y=int(x)
+# print(y)
+# z=10
+# print(float(z))
+# print(complex(z))
+# print(str(z))
+# a=p
+# print(a) not defined error
+# l=[10,20,30,40,50]
+# print(list(l))
+# print(frozenset(l))
