7664d217c79b5e123d877f42da0f17aae377d0bd
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-17 23:59:41 +0530

    hi

---
+++ Python-Fundamentals/day13_string_indexing.py
@@ -0,0 +1,11 @@
+s='this is python'
+print(s.index('i'))
+#print(s.index('I'))
+print(s.index('is'))
+# syntex = index('s' , starting , ending)
+print(s.index('p',2)) # searching start
+print(s.index('i' ,1 , 4)) # startinng and ending of searching
+
+
+
+
