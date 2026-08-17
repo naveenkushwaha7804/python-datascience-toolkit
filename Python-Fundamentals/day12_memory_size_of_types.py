c43a415a070981f7cc7544bcceb2f5e947bc1932
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-17 23:59:12 +0530

    Add Python object memory size examples

---
+++ Python-Fundamentals/day12_memory_size_of_types.py
@@ -0,0 +1,15 @@
+import sys
+
+a=int()#28
+b=float()#24
+c=str()#41
+d=complex()#32
+e=set()#216
+f=[]
+
+print(sys.getsizeof(a))
+print(sys.getsizeof(b))
+print(sys.getsizeof(c))
+print(sys.getsizeof(d))
+print(sys.getsizeof(e))
+print(sys.getsizeof(f))
