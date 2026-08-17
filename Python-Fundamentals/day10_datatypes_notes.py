1a629402f9ca9d718687abb4caf1ea440918de78
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-17 21:06:32 +0530

    Add Python integer and string fundamentals

---
+++ Python-Fundamentals/day10_datatypes_notes.py
@@ -0,0 +1,24 @@
+    #    Integer
+# x=10
+
+# 1 - print(x)
+# 2 - print(type(x))
+# 3 - print(id(x))
+# --------------------
+#    yah nahi lagenge 
+# 4 - print(max(x))
+# 5 - print(min(x))
+# 6 - print(len(x))
+
+    #   String
+
+# => collection of characters
+# => represented by '' / "" / '''''
+# => indexing supported
+# => slicing supported
+# => dublicateds are allowed
+# => immutbale inn nature
+# ex- 
+# s='print(s,type(s))
+
+     
