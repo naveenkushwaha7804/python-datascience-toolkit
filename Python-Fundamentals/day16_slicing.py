0970ab9e43daa226b3dc920592342390fedbc106
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-18 14:26:03 +0530

    Add Python slicing examples for strings and lists

---
+++ Python-Fundamentals/day16_slicing.py
@@ -0,0 +1,12 @@
+l=[10,20,30,40,50]
+print(l[ : : ])
+print(l[ : :-1])
+s='PYTHON'
+print(s[ : :-1 ])
+print(s[ : : ])
+st=input('enter a string')
+print(st[ : :-3])# 2 2 index ka gap kr dega
+print(st[ :])
+n=[10, 20 ,30,2,4,5,6]
+print(n[2:4:5])
+print(n[4:-3:4])
