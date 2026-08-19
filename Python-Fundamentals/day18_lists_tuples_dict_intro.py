e00523be78771dc3d34157722f746f6d5c8c1393
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-19 22:00:04 +0530

    Add Python examples for lists tuples and dictionaries

---
+++ Python-Fundamentals/day18_lists_tuples_dict_intro.py
@@ -0,0 +1,42 @@
+print('name \'naveen\' kushwaha')
+# cotes ke andar cotes use 
+
+# LIST 
+l1=[1,2,3,4,5,6]
+print(l1)
+l2=[2,'naveen',5,0.5]
+print(l2)
+
+print(type(l1) , type(l2))
+
+#Empty list
+
+l3=[]
+print(l3 , type(l3))
+l4=list()
+print(l4 , type(l4))
+
+#Touples
+
+t=('ele1' , 'ele2' ,'ele3' )
+t1=(2 , 4 , 5 , 6)
+t2=(2 , 2.5 , 'python')
+print(t,t1,t2 ,type(t),type(t1),type(t2))
+
+# types
+y=10,
+print(y,type(y))
+z=(10)
+print(z, type(z))
+r=(10,)
+print(r,type(r))
+
+#DICTIONARY
+
+d={'name':'naveen' , 'age':20}
+print(d,type(d))
+d1={}
+print(d1,type(d))
+
+
+
