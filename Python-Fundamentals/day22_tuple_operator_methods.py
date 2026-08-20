d58073914fa60220befdcfc75656df6550e89f16
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-20 23:56:44 +0530

    Add Python tuple functions and methods examples

---
+++ Python-Fundamentals/day22_tuple_operator_methods.py
@@ -0,0 +1,44 @@
+# TUPLE (IMMUTABLE)
+
+# Collection of elements
+# Represented by () with comma (,)
+# ---- etc.
+
+t = (1, 2, 3, 4, 5, 'python', 'naveen')
+
+# print(t)
+# print(type(t))
+
+# Inbuilt functions (supported)
+# len()
+# type()
+# id()
+# print()
+
+# Not supported for heterogeneous tuples only
+# All supported for homogeneous tuples
+
+# max()
+# min()
+# sum()
+
+# print(len(t))
+# print(type(t))
+# print(id(t))
+
+# -----------------
+
+# print(max(t))
+# print(min(t))
+# print(sum(t))
+
+
+# METHODS
+
+print(t.index(3))
+print(t.count('naveen'))
+
+
+# OPERATOR
+
+# HW: Tuple operators
