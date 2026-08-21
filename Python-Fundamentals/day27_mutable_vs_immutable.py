6c6fbcbfe46f2c6f477f19b63f40033c50119cb7
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-21 16:26:50 +0530

    Add Python mutable and immutable data type examples

---
+++ Python-Fundamentals/day27_mutable_vs_immutable.py
@@ -0,0 +1,24 @@
+#Mutable(changable)
+#immutable(not changable)
+
+# Mutables are
+
+#Set(Unordered)
+#List
+#dictionary
+
+#immutable
+
+#integer
+#Float
+#String
+#boolean
+#touple
+#frozen set
+#complex
+#
+
+my_set=(10 , 10 , 30 , 'python', 'java')
+my_set1=(10 , 10 , 30 , 'python', 'java')
+print(id(my_set) , id (my_set1))
+
