cffc0ab24d8c77257bc6cec100b5afd43ca9bbb4
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-24 21:53:57 +0530

    Add Python frozenset creation and set operation examples

---
+++ Python-Fundamentals/day28_membership_identity_operator.py
@@ -0,0 +1,23 @@
+#    Types =  in , not in
+s='a e i o u A E I O U'
+s1='ie'
+s2='ei'
+# print(s1 in s)
+# print(s in s1)
+# print(s2 in s)
+# print(s in s2)
+
+# ----------------------------
+# sequence matter karta hai
+
+ #Identity
+
+s3='python'
+s4='java'
+s5='python'
+# print(s3 is s4)
+# print(s3 is s5) # memory ke base pe check karta hai
+
+# ------------------------------
+
+#    
