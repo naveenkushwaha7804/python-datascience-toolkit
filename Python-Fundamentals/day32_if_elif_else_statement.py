7ae28b584a22134105075307e7809b7a6e54367e
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-25 18:09:45 +0530

    Add positive negative and zero number check

---
+++ Python-Fundamentals/day32_if_elif_else_statement.py
@@ -0,0 +1,10 @@
+x=int(input("enter a no."))
+if x>0:
+   print(f'given no. {x} is positive')
+elif x<0:
+   print(f'enter no. {x} is negative')
+#elif x==0:
+#   print(f'given no. {x} is zero')
+else:
+   print(f'given no. {x} is zero')
+ 
