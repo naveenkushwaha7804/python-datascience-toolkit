239f8c2e8a9d2b111c5e9aacc710362299a8596d
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-08-30 22:04:51 +0530

    correct swapped homogeneous/heterogeneous labels + handle sum() error
    - Wrapped print(sum(s)) in try/except to demonstrate the TypeError safely
    - Fixed comment labels: l (mixed types) is Heterogeneous, l2 (all int) is Homogeneous
    - Organized inbuilt function demos for strings and lists

---
+++ Python-Fundamentals/day46_inbuilt_functions.py
@@ -0,0 +1,36 @@
+# inbuilt finctions in python 
+
+s='Python'
+
+#   SUPPORTED
+
+# 1- print() -> print(s)
+# 2- type()  -> print(type(s))
+# 3- id()    -> print(id(s))
+# 4- len()   -> print(len(s)) -6
+# 5- max()   -> print(max(s)) -y
+# 6- min()   -> print(min(s))-p
+# 7- sum()   -> print(sums(S))
+
+#  NOT SUPPORTED
+print(sum(s))  #error because string didn't allow sum operation
+# -----------------------------
+
+l=[10,20,'python','java',40] #HOMOGENIOUS    
+l2=[10,20,30,40] #hACTROGENIOUS
+
+
+#   l will support -
+ # 1- print() -> print(s)
+# 2- type()  -> print(type(s))
+# 3- id()    -> print(id(s))
+# 4- len()   -> print(len(s)) 
+
+# l2 will support
+
+# 1- print() -> print(s)
+# 2- type()  -> print(type(s))
+# 3- id()    -> print(id(s))
+# 4- len()   -> print(len(s)) 
+# 5- max()   -> print(max(s)) 
+# 6- min()   -> print(min(s))
