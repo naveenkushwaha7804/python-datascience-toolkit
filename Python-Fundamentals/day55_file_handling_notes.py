1400afeae089bedc870bf44f6875e68f2fcac371
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2026-09-01 18:08:23 +0530

    Add notes: file handling operations and modes (create, write, read, append)

---
+++ Python-Fundamentals/day55_file_handling_notes.py
@@ -0,0 +1,17 @@
+'''
+OPERATIONS
+
+-Create()/open()
+-Write()/read()
+-Close()
+
+1. Open:-
+
+Syntex = open(file_name with extention , mode )
+
+mode =
+        x=create
+        w=write
+        r=read
+        a=append
+'''
